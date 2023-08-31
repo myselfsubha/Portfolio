55c7ad2e39706471cdd14cb87a0805b8ccdac067
myselfsubha <fullscreen.abc@gmail.com>
2023-08-31 09:11:30 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// var Typed= new Typed(".text",{
+//     Strings: ["Frontend Developer" , "Python Developer" , "Web Developer"],
+//     typeSpeed: 10,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop:true
+// });
+
+var typed = new Typed('#existing-text', {
+strings: ["Frontend Developer" , "Python Developer" , "Web Developer"],
+backSpeed: 80,
+typeSpeed: 80,
+loop: true,
+startDelay: 100,
+backDelay: 500,            
+});
